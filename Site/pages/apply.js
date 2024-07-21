5ef9db2a1dd5840696b5cd43580f41931d1bff42
Mohit Panjikar <20210802090@dypiu.ac.in>
2024-07-21 23:21:57 +0530

    working on the landing page

--- Site/pages/apply.js
+++ Site/pages/apply.js
@@ -1,24 +1,47 @@
-import React from 'react'
-import styles from '../styles/apply.module.css'
+import React from "react";
+import styles from "../styles/apply.module.css";
 
 const Apply = () => {
   return (
     <>
-      <section className=''>
-        <div  className={styles.background + " main min-h-screen flex justify-center items-center"}>
-          <div className="content form">
-              <h1 className="text-2xl font-bold text-white text-center pb-5">Apply for NewsLetter</h1>
-              <div className="">
-                  <form className="">
-                      <input className="border-1 border-gray-100 bg-white text-black shadow-lg p-2 rounded-l-lg focus:outline-none autofocus" type="email" placeholder='Enter your email' required/>
-                      <input className="bg-indigo-600 text-white p-2 rounded-r-lg" type="submit" value="Subscribe" />
-                  </form>
-              </div>
+      <section
+        className={`${styles.background} min-h-screen flex justify-center items-center`}
+      >
+        <div className="max-w-4xl w-full px-4">
+          <div className="main">
+            <div className="content bg-white p-8 rounded-lg shadow-lg">
+              <h1 className="text-2xl font-bold text-center mb-8">
+                Join the top 1% creators
+              </h1>
+              <p className="text-center">Create LinkTree for Brand</p>
+              <form className="flex flex-col gap-4">
+                <div className="flex items-center gap-2">
+                  <img className="w-6 mr-2" src="/svg/ig.svg" alt="" />
+                  <input
+                    className="shadow-md border-2 px-3 py-2 rounded-md focus:outline-none"
+                    type="text"
+                    placeholder="Social Handle"
+                  />
+                </div>
+
+                <input
+                  className="shadow-md border-2 px-3 py-2 rounded-md focus:outline-none"
+                  type="email"
+                  placeholder="Enter your email"
+                />
+
+                <input
+                  className="shadow-md border-2 px-3 py-2 rounded-md focus:outline-none"
+                  type="password"
+                  placeholder="Enter your password"
+                />
+              </form>
+            </div>
           </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Apply
+export default Apply;
